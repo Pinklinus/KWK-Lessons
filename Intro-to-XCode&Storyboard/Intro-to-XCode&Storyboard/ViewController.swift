aeeadda346199aa2b52eb57284bf9dc743499199
Pinklinus <87437095+Pinklinus@users.noreply.github.com>
2021-07-14 11:42:15 -0400

    Add files via upload
    Intro to XCode & Storyboard Version 1

---
+++ Intro-to-XCode&Storyboard/Intro-to-XCode&Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intro-to-XCode&Storyboard
+//
+//  Created by Rachel F on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing testing!")
+    }
+
+
+}
+
