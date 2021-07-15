3c94efa9e80b8337f014210792ea19f9e37731f2
Pinklinus <87437095+Pinklinus@users.noreply.github.com>
2021-07-15 17:02:08 -0400

    Add files via upload
    Version 1

---
+++ MiniProject2/MiniProject2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniProject2
+//
+//  Created by Rachel F on 7/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var score: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
---
+++ MiniProject2/MiniProject2/thirdQuestion.swift
@@ -0,0 +1,29 @@
+//
+//  thirdQuestion.swift
+//  MiniProject2
+//
+//  Created by Rachel F on 7/15/21.
+//
+
+import UIKit
+
+class thirdQuestion: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
